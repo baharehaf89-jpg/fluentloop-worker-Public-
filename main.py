2ce184347e528a1b679a0085b1a9e96017d930bf
baharehaf89-jpg <bahareh.af89@gmail.com>
2026-05-17 14:20:35 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-import requests
-import os
 
 app = FastAPI()
 
-OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
-
-class ChatRequest(BaseModel):
-    message: str
-
 @app.get("/")
 def home():
-    return {"message": "FluentLoop AI is running"}
-
-@app.post("/chat")
-def chat(req: ChatRequest):
-
-    response = requests.post(
-        "https://openrouter.ai/api/v1/chat/completions",
-        headers={
-            "Authorization": f"Bearer {OPENROUTER_API_KEY}",
-            "Content-Type": "application/json"
-        },
-        json={
-            "model": "openai/gpt-4o-mini",
-            "messages": [
-                {
-                    "role": "user",
-                    "content": req.message
-                }
-            ]
-        }
-    )
-
-    data = response.json()
-
-    return {
-        "reply": data["choices"][0]["message"]["content"]
-    }
-
-# update
+    return {"HELLO": "NEW VERSION"}
